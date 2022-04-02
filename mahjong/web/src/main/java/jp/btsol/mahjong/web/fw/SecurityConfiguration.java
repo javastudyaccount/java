096ac479d1bdc9388ff92b6b422e76c561ab2bc5
systemi.repository.developer@systemi.co.jp-at-754454770700 <wzhang@systemi.co.jp>
2022-04-02 21:31:33 +0900

    login

--- mahjong/web/src/main/java/jp/btsol/mahjong/web/fw/SecurityConfiguration.java
+++ mahjong/web/src/main/java/jp/btsol/mahjong/web/fw/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package jp.btsol.mahjong.web.fw;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,7 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/image/**", "/js/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/signin", "/login").permitAll()//
+                .antMatchers("/admin/**").hasRole("ADMIN")//
+                .anyRequest().authenticated();
+        http.sessionManagement().invalidSessionUrl("/login");
     }
 }
